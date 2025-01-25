1ddf355d734f35d2318795a016199fa77378325a
Prabin Babu Kattel <91269916+dailydoseofgithub@users.noreply.github.com>
2025-01-25 20:56:42 +0545

    Add files via upload

---
+++ RMS/src/RMS.java
@@ -0,0 +1,24 @@
+
+import Login.Login;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author user
+ */
+
+public class RMS {
+
+    public static void main(String[] args) {
+        
+        Login lg = new Login();
+        lg.setVisible(true);
+        System.out.println("Hello World!");
+        
+        //use libraray from this java
+        
+    }
+}
